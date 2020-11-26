6e3367f1390a535b908598387730ea76c1f09678
Ajeet <ajeet_athwal@hotmail.co.uk>
2020-11-26 20:00:37 +0000

    refactored code into more modules

--- script.js
+++ script.js
@@ -1,48 +1,100 @@
-const Gameboard = ((doc) => {
-    const rowNumber = 3;
-    const columnNumber = 3;
-    const Player = (symbol) => {
-        return {symbol};
+const TurnHandler = (() => {
+    // cache
+    const symbol1 = "X";
+    const symbol2 = "O";
+
+    let turn = symbol1;
+
+    // functions
+
+    const playTurn = () => {
+        const temp = turn;
+        turn = turn === symbol1 ? symbol2 : symbol1;
+        return temp;
     }
 
-    const player1 = Player("X");
-    const player2 = Player("O");
+    const getCurrentTurn = () => {
+        return turn;
+    }
 
-    let turn = player1.symbol;
-    
+    const getSymbol1 = () => {
+        return symbol1;
+    }
+
+    const getSymbol2 = () => {
+        return symbol2;
+    }
+
+    const changeFirstTurn = () => {
+        if (Gameboard.isEmpty()) turn = turn === symbol1 ? symbol2 : symbol1;
+    }
+
+
+
+    return {playTurn, getCurrentTurn, getSymbol1, getSymbol2, changeFirstTurn};
+})();
+
+const Gameboard = (() => {
+    // cache
     const gameboard = [ 
         ["","",""],
         ["","",""],
         ["","",""]
     ];
+    const gridSize = 3;
 
-    const playTurn = (e) => {
-        const row = e.target.dataset.row;
-        const column = e.target.dataset.column;
-        gameboard[row][column] = turn;
-        turn = turn === player1.symbol ? player2.symbol : player1.symbol;
+    
+    // functions
+
+    const playTurn = (row, column, turn) => {
+        if (gameboard[row][column] === "") gameboard[row][column] = turn;
+    }
+
+    const clearGameboard = () => {
+        for (let row = 0; row < gridSize; row++)
+            for (let column = 0; column < columnNumber; column++)
+            gameboard[row][column] = "";
+    }
+
+    const reset = (firstTurn) => {
         clearGameboard();
-        displayGameboard();
     }
 
-
-    const pretendTurn = (e) => {
-        const row = e.target.dataset.row;
-        const column = e.target.dataset.column;
-        
-        if (e.target.innerText === "" ){
-            e.target.innerText = turn;
-            e.target.classList.add("temp-input");
-        }
-        else {
-            e.target.innerText = "";
-            e.target.classList.remove("temp-input");
-        }
+    const isEmpty = () => {
+        for (let row = 0; row < gridSize; row++)
+            for (let column = 0; column < columnNumber; column++)
+                if (gameboard[row][column] !== "") return false;
+        return true;
     }
 
+    const getGridSize = () => gridSize;
+
+    const getCellInput = (row, column) => gameboard[row][column];
+
+    return {reset, isEmpty, playTurn, getGridSize, getCellInput};
+
+})();
+
+const Player = (name, symbol) => {
+    const setName = (newName) => name = newName;
+
+    const getName = () => name;
+    const getSymbol = () => symbol;
+
+    return {setName, getName, getSymbol};
+};
+
+const player1 = Player("Player 1", TurnHandler.getSymbol1());
+const player2 = Player("Player 2", TurnHandler.getSymbol2());
+
+const domHandler = ((doc) => {
+    const htmlBoard = doc.querySelector("#board");
+
+    // EventHandlers
     const addCell = (htmlBoard, row, column) => {
-        const cell = document.createElement("div");
-        const entry = gameboard[row][column]
+        const cell = doc.createElement("div");
+        
+        const entry = Gameboard.getCellInput(row, column);
 
         cell.dataset.row = row;
         cell.dataset.column = column;
@@ -50,36 +102,52 @@ const Gameboard = ((doc) => {
         cell.innerText = entry;
         if (entry === "") {
             cell.addEventListener("click", playTurn);
-            cell.addEventListener("mouseover", pretendTurn);
-            cell.addEventListener("mouseout", pretendTurn);
+            cell.addEventListener("mouseover", hoverCell);
+            cell.addEventListener("mouseout", hoverCell);
         }
 
         htmlBoard.appendChild(cell);
     }
 
-    const displayGameboard = () => {
-        const htmlBoard = doc.querySelector("#board");
-        for (let row = 0; row < rowNumber; row++)
-            for (let column = 0; column < columnNumber; column++)
-                addCell(htmlBoard, row, column);
+    const playTurn = (e) => {
+        const row = e.target.dataset.row;
+        const column = e.target.dataset.column;
+
+        Gameboard.playTurn(row, column, TurnHandler.playTurn());
+        render();
     }
 
-    const clearGameboard = () => {
-        const htmlBoard = doc.querySelector("#board");
+    const hoverCell = (e) => {
+        const cell = e.target;
+        const currentTurn = TurnHandler.getCurrentTurn();
+
+        if (cell.innerText === "" ){
+            cell.innerText = currentTurn;
+            cell.classList.add("temp-input");
+        }
+        else {
+            cell.innerText = "";
+            cell.classList.remove("temp-input");
+        }
+    }
+
+    // render
+
+    const render = () => {
+        clearGameboardHtml();
+        displayGameboardHtml();
+    }
+
+    const clearGameboardHtml = () => {
         while (htmlBoard.firstChild) htmlBoard.removeChild(htmlBoard.firstChild);
     }
 
-    const displayPlayer1Symbol = () => {
-        return player1.symbol;
+    const displayGameboardHtml = () => {
+        for (let row = 0; row < Gameboard.getGridSize(); row++)
+            for (let column = 0; column < Gameboard.getGridSize(); column++)
+                addCell(htmlBoard, row, column);
     }
 
-    const displayPlayer2Symbol = () => {
-        return player2.symbol;
-    }
+    render();
 
-    return {displayGameboard, displayPlayer1Symbol, displayPlayer2Symbol};
-
-})(document)
-
-
-Gameboard.displayGameboard();
+})(document);
